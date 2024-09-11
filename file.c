162808d316e29c7d255f3d20cff7c86db7a0afd7
Alexander Sokoler <alexandersokoler@gmail.com>
2024-09-11 11:54:21 +0200

    main changed to api (2) and fileFormatInspector to inspection only (3)

--- file.c
+++ file.c
@@ -3,44 +3,43 @@
 #include <string.h> // strerror.
 #include <errno.h>  // errno.
 
-int fileFormatInspector(char *filename) {
-        int retval = EXIT_FAILURE;
-        FILE *file = fopen(filename, "r");
-        //Not sure if needed
+int fileFormatInspector(FILE *file_p){
+    int retval = EXIT_FAILURE;
+    char buffer[BUFSIZ];
+    while (fgets(buffer, sizeof(buffer), file_p) != NULL) {
+        fprintf(stdout, "%s", buffer);
+        for (unsigned long i = 0; i < strlen(buffer); i++) {
+            if ((unsigned char)buffer[i] > 0x7F && (unsigned char)buffer[i] < 0xFF) {
+                printf("This is a UTF8-*file\n");
+                break;
+                }
+            else if ((unsigned char)buffer[i] > 0x7F) {
+                printf("This is not an ascii *file\n"); 
+                break;
+                };
+            };            
+    };
+        printf("\n");
+        retval = EXIT_SUCCESS;
+        return retval;
+}
+
+
+int main(int argc, char *argv[]) {
+    int retval = EXIT_FAILURE;
+    if (argc == 2){
+        FILE *file = fopen(argv[1], "r");
         if (file == NULL) {
-            int retval = EXIT_FAILURE;
+            fclose(file);
             return retval;
         }
         else {
-            char buffer[BUFSIZ];
-            while (fgets(buffer, sizeof(buffer), file) != NULL) {
-                fprintf(stdout, "%s", buffer);
-                for (unsigned long i = 0; i < strlen(buffer); i++) {
-                    if ((unsigned char)buffer[i] > 0x7F && (unsigned char)buffer[i] < 0xFF) {
-                        printf("This is a UTF8-file\n");
-                        break;
-                    }
-                    else if ((unsigned char)buffer[i] > 0x7F) {
-                        printf("This is not an ascii file\n"); 
-                        break;
-                    };
-                    
-                };
-            };
-        printf("\n");
-        retval = EXIT_SUCCESS;
-        fclose(file);
-        return retval;
-        };
-}
-
-int main(int argc, char *argv[]) {
-    int retval = EXIT_SUCCESS;
-    if (argc == 2){
-        retval = fileFormatInspector(argv[1]);
+            retval = fileFormatInspector(file);
+            fclose(file);
+        }
     }    
     else {
-        retval = EXIT_FAILURE;
+        return retval;
     }
     return retval;
 }
