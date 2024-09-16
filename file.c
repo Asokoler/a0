64e2cc406fc7fd45d622179a906855736cc0057e
Alexander Sokoler <alexandersokoler@gmail.com>
2024-09-16 16:35:09 +0200

    file.c returns correct value and is running in test.sh

--- file.c
+++ file.c
@@ -4,11 +4,12 @@
 #include <errno.h>  // errno.
 #include <unistd.h> // access, F_OK, R_OK.
 
-
+//Note: Not checking: 
+//if (file == NULL) {return 0;}
+//Not sure if needed
 
 int print_error(char *path, int errnum) {
-    fprintf(stdout, "%s: cannot determine (%s)\n",
-    path, strerror(errnum));
+    fprintf(stdout, "%s: cannot determine (%s)\n", path, strerror(errnum));
     return 0;
 }
 
@@ -19,104 +20,84 @@ int print_type(int type, const char* path) {
         "UTF-8 Unicode text",
         "data"
     };
-    printf("./%s\n%sfile: %s\n", path, FILE_TYPE_STRINGS[type], FILE_TYPE_STRINGS[type]);
+    printf("%s: %s\n", path, FILE_TYPE_STRINGS[type]);
     return 0;
 }
 
-int inspectEmpty(const char* path){
+int inspectEmpty(const char* path) {
     FILE *file = fopen(path, "r");
     int ch = fgetc(file);
     if (ch != EOF) {
+        fclose(file);
         return 0; 
-        } 
+    } 
     fclose(file);
     return 1;
 }
 
-int instpectASCII(const char* path){
+int inspectASCII(const char* path) {
     FILE *file = fopen(path, "r");
-    //{0x07, 0x08, . . . 0x0D} ∪ {0x1B} ∪ {0x20, 0x21, . . . , 0x7E}
     int ch = fgetc(file);
     while (ch != EOF) {
         if ((ch >= 0x07 && ch <= 0x0D) || ch == 0x1B || (ch >= 0x20 && ch <= 0x7E)) {
             ch = fgetc(file);
-        }
-        else {
+        } else {
+            fclose(file);
             return 0;
         }
-    };
+    }
+    fclose(file);
     return 1;
 }
 
-int inspectISOinASCII(const char* path){
+int inspectISOinASCII(const char* path) {
     FILE *file = fopen(path, "r");
-    //128–159
     int ch = fgetc(file);
     while (ch != EOF) {
         if (ch >= 128 && ch <= 159) {
             ch = fgetc(file);
-        }
-        else {
+        } else {
+            fclose(file);
             return 0;
         }
-    };   
+    }
+    fclose(file);
     return 1;
-} 
-
-//int inspectUTF8(const char* path){
-//    if (access(path, R_OK) != 0) {
-//        return 3;
-//    }
-//    return 0;
-//}
+}
 
 int main(int argc, char *argv[]) {
     int retval = EXIT_FAILURE;
-    if (argc == 1){
+    if (argc == 1) {
         fprintf(stderr, "Usage: file path\n");
-    }
-
-    else if (argc == 2){
+    } else if (argc == 2) {
         if (access(argv[1], F_OK) != 0) {
-            retval = print_error(argv[1],ENOENT);
-        }
-
-        else if (access(argv[1], R_OK) != 0) {
-            retval = print_error(argv[1],EACCES);
-        }
-
-        else if ((access(argv[1], R_OK) == 0) && (access(argv[1], F_OK) == 0)) {
+            retval = print_error(argv[1], ENOENT);
+        } else if (access(argv[1], R_OK) != 0) {
+            retval = print_error(argv[1], EACCES);
+        } else if ((access(argv[1], R_OK) == 0) && (access(argv[1], F_OK) == 0)) {
             int is_empty = inspectEmpty(argv[1]);
-            int is_ascii = instpectASCII(argv[1]);
+            int is_ascii = inspectASCII(argv[1]);
             int is_iso = inspectISOinASCII(argv[1]);
-            //int is_utf8 = inspectUTF8(argv[1]);
 
-            if (is_empty){
+            if (is_empty) {
                 print_type(0, argv[1]);
-            }
-            else if (is_ascii){
-                if (is_iso){
+                retval = EXIT_SUCCESS;
+            } else if (is_ascii) {
+                if (is_iso) {
                     print_type(2, argv[1]);
-                }
-                else {
+                } else {
                     print_type(1, argv[1]);
                 }
-            }
-            //else if (is_utf8){
-            //    print_type(3, argv[1]);
-            //}
-            else {
+                retval = EXIT_SUCCESS;
+            } else {
                 print_type(3, argv[1]);
+                retval = EXIT_SUCCESS;
             }
+        } else {
+            fprintf(stderr, "Error: Something went wrong while reading the file. Not sure what...\n");
         }
-        else {
-            fprintf(stderr, "Error: Something went wrong while reading the file. Not sure what... \n");
-        }
-
-    }
-    else{
+    } else {
         fprintf(stderr, "Usage: Too many arguments provided.\n");
     }
-    
     return retval;
 }
