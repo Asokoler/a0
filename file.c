ce2811b16a28778ba1e00837e77738026169d6bd
Alexander Sokoler <alexandersokoler@gmail.com>
2024-09-18 15:09:14 +0200

    file.c working with utf8

--- file.c
+++ file.c
@@ -16,6 +16,7 @@ int print_error(char *path, int errnum) {
 int print_type(int type, const char* path) {
     const char * const FILE_TYPE_STRINGS[] = {
         "empty",
+        "ISO-8859 text",
         "ASCII text",
         "UTF-8 Unicode text",
         "data"
@@ -26,6 +27,10 @@ int print_type(int type, const char* path) {
 
 int inspectEmpty(const char* path) {
     FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", path);
+        return 0;
+    }
     int ch = fgetc(file);
     if (ch != EOF) {
         fclose(file);
@@ -37,6 +42,10 @@ int inspectEmpty(const char* path) {
 
 int inspectASCII(const char* path) {
     FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", path);
+        return 0;
+    }
     int ch = fgetc(file);
     while (ch != EOF) {
         if ((ch >= 0x07 && ch <= 0x0D) || ch == 0x1B || (ch >= 0x20 && ch <= 0x7E)) {
@@ -52,6 +61,10 @@ int inspectASCII(const char* path) {
 
 int inspectISOinASCII(const char* path) {
     FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", path);
+        return 0;
+    }
     int ch = fgetc(file);
     while (ch != EOF) {
         if (ch >= 128 && ch <= 159) {
@@ -67,33 +80,44 @@ int inspectISOinASCII(const char* path) {
 
 int inspectUTF(const char* path) {
     FILE *file = fopen(path, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", path);
+        return 0;
+    }
     int ch = fgetc(file);
     int byteCounter = 1;
     while (ch != EOF) {
         if ((ch <= 127) && (ch > 0)) {
                 byteCounter = 1;
-            }
-            else if ((ch <= 223) && (ch >= 192)) {
+        }
+        else if ((ch <= 223) && (ch >= 192)) {
                 byteCounter = 2;
-            }
-            else if ((ch <= 239) && (ch >= 224)) {
+        }
+        else if ((ch <= 239) && (ch >= 224)) {
                 byteCounter = 3;
-            }
-            else if ((ch >= 240) && (ch <= 247)) {
+        }
+        else if ((ch >= 240) && (ch <= 247)) {
                 byteCounter = 4;
+        }
+        else{
+            fclose(file);
+            return 0;
+        }
+        
+        for (int i = 0; i < (byteCounter-1); i++) {
+            ch = fgetc(file);
+            if ((ch <= 191) && (ch >= 128)) {
+                continue;
             }
-            for (int i = 0; i < (byteCounter-1); i++) {
-                ch = fgetc(file);
-                if ((ch <= 191) && (ch >= 128)) {
-                    return 1;
-                }
-                else {
-                    return 0;
-                    break;
-                }
-        fclose(file);
-        return 1;
-    }
+            else {
+                fclose(file);
+                return 0;
+            }
+        }
+        ch = fgetc(file);
+    };
+    fclose(file);
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -109,21 +133,25 @@ int main(int argc, char *argv[]) {
             int is_empty = inspectEmpty(argv[1]);
             int is_ascii = inspectASCII(argv[1]);
             int is_iso = inspectISOinASCII(argv[1]);
+            int is_utf = inspectUTF(argv[1]);
 
             if (is_empty) {
                 print_type(0, argv[1]);
-                retval = EXIT_SUCCESS;
-            } else if (is_ascii) {
+            } 
+            else if (is_ascii) {
                 if (is_iso) {
-                    print_type(2, argv[1]);
-                } else {
                     print_type(1, argv[1]);
+                } else {
+                    print_type(2, argv[1]);
                 }
-                retval = EXIT_SUCCESS;
-            } else {
+            } 
+            else if (is_utf) {
                 print_type(3, argv[1]);
-                retval = EXIT_SUCCESS;
+            } 
+            else {
+                print_type(4, argv[1]);
             }
+            retval = EXIT_SUCCESS;
         } else {
             fprintf(stderr, "Error: Something went wrong while reading the file. Not sure what...\n");
         }
